7837bde4dc7cd3f12d2c58fb13aea630848f8bd4
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2023-12-09 21:13:13 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,9 +8,8 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNjA4MmFiYjdiYTRlZWUyMzNiMzc1MGVlNzI2MTZkOCIsInN1YiI6IjY1NzFmMWUxODU4Njc4MDEyYzkyYjU2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EvjP1T737hyPAA3rWdLnVFfi98hERzdghM-_Br_ygGY",
-  },
+    Authorization: "Bearer " + process.env.REACT_APP_TMDB_KEY,
+      
 };
 
 export const IMG_CDN_URL = " https://image.tmdb.org/t/p/w500";
