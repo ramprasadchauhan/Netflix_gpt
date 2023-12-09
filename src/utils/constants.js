9d5208cfa3151e0ec1bec6ca3d61f858160021dd
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2023-12-09 21:09:21 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,7 +9,7 @@ export const API_OPTIONS = {
   headers: {
     accept: "application/json",
     Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNjA4MmFiYjdiYTRlZWUyMzNiMzc1MGVlNzI2MTZkOCIsInN1YiI6IjY1NzFmMWUxODU4Njc4MDEyYzkyYjU2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EvjP1T737hyPAA3rWdLnVFfi98hERzdghM-_Br_ygGY",
+     "Bearer " + process.env.REACT_APP_TMDB_KEY,
   },
 };
 
